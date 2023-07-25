c1a0cc7ad75eb3ed64bd6e745c6309c7365069ed
Lee, HuiJu <leemoon63@gmail.com>
2023-07-26 03:05:19 +0900

    MVC프레임워크 만들기_어댑터패턴2

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -1,4 +1,44 @@
 package hello.servlet.web.frontcontroller.v5.adapter;
 
-public class ControllerV4HandlerAdapter {
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV4);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV4 controller = (ControllerV4) handler;
+
+
+        Map<String, String> paramMap = createParamMap(request);
+        HashMap<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
+
+        return mv;
+    }
+
+    //paraMap 가져오기
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paraMap = new HashMap<String, String>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paraMap.put(paramName, request.getParameter(paramName)));
+        return paraMap;
+    }
 }
