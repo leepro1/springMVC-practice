73581dc69b2fa98371a77cece038e266d922483e
Lee, HuiJu <leemoon63@gmail.com>
2023-08-22 21:29:31 +0900

    회원 게시판 예제 최초 커밋

---
+++ mvc-study/src/test/java/um/study/MvcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package um.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
