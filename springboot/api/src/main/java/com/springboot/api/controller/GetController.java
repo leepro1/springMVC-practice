0e63202059e4fdc7673e969ad7b8f295a4c7f251
Huiju Lee <leemoon63@gmail.com>
2023-09-28 23:42:04 +0900

    core springboot api_GET

---
+++ springboot/api/src/main/java/com/springboot/api/controller/GetController.java
@@ -0,0 +1,41 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.dto.MemberDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+
+    @GetMapping("/name")
+    public String getName() {
+        return "Lee";
+    }
+
+    @GetMapping("/id/{id}")
+    public String getId(@PathVariable Long id) {
+        return "id: " + id;
+    }
+
+    @GetMapping("/request1")
+    public String getRequestParam1(@RequestParam String name, @RequestParam Long id) {
+        return "name: " + name + " id: " + id;
+    }
+
+    @GetMapping("/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> paramMap) {
+        StringBuilder sb = new StringBuilder();
+        paramMap.entrySet().forEach(map -> {
+            sb.append(map.getKey() + ": " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @GetMapping("/request3")
+    public String getRequestParam3(MemberDTO memberDTO) {
+        return memberDTO.toString();
+    }
+}
---
+++ springboot/api/src/main/java/com/springboot/api/dto/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.springboot.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+    private String name;
+    private Long id;
+}
