ccf4130863cd6193eb0cbd6df50acafdd7e6bc79
Huiju Lee <leemoon63@gmail.com>
2023-09-30 23:42:34 +0900

    core springboot jpa_dao 생성

---
+++ springboot/jpa/src/main/java/com/springboot/jpa/dao/ProductDAOImpl.java
@@ -0,0 +1,66 @@
+package com.springboot.jpa.dao;
+
+import com.springboot.jpa.entity.Product;
+import com.springboot.jpa.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Component
+public class ProductDAOImpl implements ProductDAO {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductDAOImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product insertProduct(Product product) {
+        Product savedProduct = productRepository.save(product);
+
+        return savedProduct;
+    }
+
+    @Override
+    public Product selectProduct(Long id) {
+        Optional<Product> selectedProduct = productRepository.findById(id);
+
+        return selectedProduct.get();
+    }
+
+    @Override
+    public Product updateProductName(Long number, String name) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        Product updatedProduct;
+        if (selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            product.setName(name);
+            product.setUpdateTime(LocalDateTime.now());
+
+            updatedProduct = productRepository.save(product);
+        } else {
+            throw new Exception();
+        }
+
+        return updatedProduct;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        if (selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            productRepository.delete(product);
+        } else {
+            throw new Exception();
+        }
+    }
+}
