90232726307db94dd5d4f7cd0193bea1612684ed
Huiju Lee <leemoon63@gmail.com>
2023-11-16 23:11:38 +0900

    core springboot relationship_OneToMany Mapping

---
+++ springboot/relationship/src/main/java/com/soringboot/relationship/Entity/CategoryEntity.java
@@ -0,0 +1,28 @@
+package com.soringboot.relationship.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String code;
+
+    private String name;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    private List<ProductEntity> products = new ArrayList<>();
+}
