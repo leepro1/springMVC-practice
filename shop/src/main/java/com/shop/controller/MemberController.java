02678aecb32e14218749a5ec39077f1d3d16b35d
Huiju Lee <leemoon63@gmail.com>
2023-11-30 00:56:03 +0900

    회원가입 유효성 검사 추가

--- shop/src/main/java/com/shop/controller/MemberController.java
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -2,9 +2,11 @@ package com.shop.controller;
 
 import com.shop.dto.MemberFormDTO;
 import com.shop.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +19,25 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/new")
-    public String memberForm(Model model){
-        model.addAttribute("memberFormDTO",new MemberFormDTO());
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDTO", new MemberFormDTO());
         return "member/memberForm";
     }
 
     @PostMapping("/new")
-    public String memberForm(MemberFormDTO memberFormDTO){
-        memberService.saveMember(memberFormDTO);
+    public String memberForm(@Valid MemberFormDTO memberFormDTO, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+
+        try {
+            memberService.saveMember(memberFormDTO);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
         return "redirect:/";
     }
 
