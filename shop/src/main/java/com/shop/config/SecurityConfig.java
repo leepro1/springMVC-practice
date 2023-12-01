2da41a0f4063b4bf708430ec0c2eed76f97a224c
Huiju Lee <leemoon63@gmail.com>
2023-12-01 22:31:25 +0900

    스프링 시큐리티 로그인/로그아웃 기능 구현

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -7,23 +7,39 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    //특정 HTTP 요청에 대한 웹 기반 보안 구성
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
-                                .requestMatchers(new MvcRequestMatcher(introspector, "/**")).permitAll()
-                                .anyRequest().authenticated()
-                );
-
+                                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+                                .anyRequest().authenticated())
+                .csrf((csrf) ->
+                        csrf
+                                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .formLogin((formLogin) ->
+                        formLogin
+                                .loginPage("/member/login") //로그인 페이지
+                                .defaultSuccessUrl("/") //로그인 성공시 이동할 URL
+                                .usernameParameter("email") //로그인 시 사용할 파라미터 이름
+                                .failureUrl("/member/login/error")) //로그인 실패시 이동할 URL
+                .logout((logout)->
+                        logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout")) //로그아웃 페이지
+                                .logoutSuccessUrl("/")) //로그아웃 성공시 이동할 URL
+        ;
         return http.build();
     }
 
--- shop/src/main/java/com/shop/service/MemberService.java
+++ shop/src/main/java/com/shop/service/MemberService.java
@@ -4,6 +4,10 @@ import com.shop.dto.MemberFormDTO;
 import com.shop.entity.Member;
 import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
@@ -25,7 +29,22 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         Member findMember = memberRepository.findByEmail(member.getEmail());
         if (findMember != null) {
-            throw new IllegalStateException("이미 가이비된 회원입니다.");
+            throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if (member == null)
+            throw new UsernameNotFoundException(email);
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
